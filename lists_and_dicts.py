8cee269780a9dda7694b46e6562f592702bf4bd1
Juan Jiménez <nefioxdev@gmail.com>
2021-11-01 20:29:10 -0600

    Listas y diccionarios

---
+++ lists_and_dicts.py
@@ -0,0 +1,28 @@
+def run():
+    my_list = [1, "Hi", True, 8.7]
+    my_dict = {'firstname': 'John', 'lastname': 'McLane'}
+
+    super_list = [
+        {'firstname': 'John', 'lastname': 'McLane'},
+        {'firstname': 'Laura', 'lastname': 'Rodelo'},
+        {'firstname': 'Rebeca', 'lastname': 'García'},
+        {'firstname': 'Pedro', 'lastname': 'Torres'},
+    ]
+
+    super_dict = {
+        "natural_nums": [1, 2, 3, 4, 5],
+        "integer_nums": [-1, -2, 0, 1, 2],
+        "floating_nums": [1.5, 3.4, -6.74, 8.21]
+    }
+
+    for key, value in super_dict.items():
+        print(key, '-', value)
+
+    for item in super_list:
+        print(item["firstname"], "-" ,item["lastname"])
+
+    for i in range(1,100):
+        print(i**i)
+
+if __name__ == '__main__':
+    run()
