866d02b1a690f0a38433c2e84e53c72113c69fc1
Juan Jiménez <nefioxdev@gmail.com>
2021-11-05 00:58:59 -0600

    Manejo de excepciones

--- debugging.py
+++ debugging.py
@@ -1,15 +1,23 @@
 def divisors(number):
-    divisors = []
-
-    for i in range(1, number +1 ):
-        if number % i == 0:
-            divisors.append(i)
-    return divisors
+    try:
+        divisors = []
+        if number < 0:
+            raise ValueError('Ingresa un número positivo')
+        for i in range(1, number +1 ):
+            if number % i == 0:
+                divisors.append(i)
+        return divisors
+    except ValueError as ve:
+        print(ve)
+        return False
 
 def run():
-    num = int(input('Ingresa un número: '))
-    print(divisors(num))
-    print('Terminó el programa')
+    try:
+        num = int(input('Ingresa un número: '))
+        print(divisors(num))
+        print('Terminó el programa')
+    except ValueError:
+        print('Debes ingresar un número')
 
 if __name__ == '__main__':
     run()
